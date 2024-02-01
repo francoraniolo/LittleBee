b6019b53ba7e1d2ffc7f21d2a055b18568ad0740
francoraniolo <franco11-09@hotmail.com>
2024-02-01 14:01:40 -0300

    Remove item feature

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -20,5 +20,26 @@ class PurchaseItemsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    @purchase = current_user.current_purchase
+    @purchase_item = @purchase.purchase_items.find(params[:id])
+    product = @purchase_item.product
+
+    to_discount = @purchase_item.total_price
+
+    @purchase.update!(total_price: @purchase.total_price - to_discount)
+    @purchase_item.destroy
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace("product_#{product.id}", partial: 'products/product', locals: { product: product }),
+          turbo_stream.update("total_price", partial: 'layouts/total_price', locals: { purchase: @purchase })
+        ]
+      end
+      format.html { redirect_to products_path, notice: 'Product removed from current purchase.' }
+    end
+  end
 end
 
